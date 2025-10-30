0a970781e0a56e88560afc82f507f3c80177bcd5
OrGutman <or.gutman7@gmail.com>
2025-10-30 13:35:14 +0200

    Update math_utils.py
    changed mean_number()

--- math_utils.py
+++ math_utils.py
@@ -9,16 +9,9 @@ def find_max_number(num1, num2, num3):
     else:
         return num3
 
-def find_min_number(num1, num2, num3):
-    if num1 < num2:
-        if num1 < num3:
-            return num1
-        else:
-            return num3
-    elif num2 < num3:
-        return num2
-    else:
-        return num3
+def find_mean_number(num1, num2, num3):
+    mean = (num1 + num2 + num3)/3
+    return mean
 
 def find_mean_std(num1, num2, num3):
     mean = find_mean(num1, num2, num3)
